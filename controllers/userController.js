fecf42cc84b1dfcfae8262ada39a634ab07eb318
devendra nagar <devendranagar853@gmail.com>
2024-02-25 15:43:40 +0530

    change usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,21 +5,25 @@ import jwt from 'jsonwebtoken'
 class UserController {
   static userRegistration = async (req, res) => {
 
-    const { name, email, password, password_confirmation } = req.body
+    const { name,  UserName, email, password, password_confirmation } = req.body
 
-    console.log({ name, email, password, password_confirmation, body: req.body })
+    console.log({ name, UserName, email, password, password_confirmation, body: req.body })
 
     const user = await UserModel.findOne({ email: email })
 
     if (user) {
+       
+      res.status(403).send({
+        message: "Records show this email is linked to another account."
+      });
 
-      res.send({ "status": "failed", "message": "Email already exists" })
+      
 
     }
 
     else {
 
-      if (name && email && password && password_confirmation) {
+      if (name && UserName && email && password && password_confirmation) {
 
         if (password === password_confirmation) {
 
@@ -32,6 +36,7 @@ class UserController {
 
             const doc = new UserModel({
               name: name,
+              UserName:UserName,
               email: email,
               password: hashPassword,
               
@@ -43,17 +48,17 @@ class UserController {
 
             // Generate JWT Token
 
-            // const token = jwt.sign({ userID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+            const token = jwt.sign({ userID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
 
-            // res.status(201).send({ "status": "success", "message": "Registration Success", "token": token })
+            res.status(201).send({ "status": "success", "message": "Registration Success", "token": token })
 
           } catch (error) {
             console.log(error)
 
-            res.send({ "status": "failed", "message": "Unable to Register" })
+            res. status(400).send({ "status": "failed", "message": "Unable to Register" })
           }
         } else {
-          res.send({ "status": "failed", "message": "Password and Confirm Password doesn't match" })
+          res.status(). send({ "status": "failed", "message": "Password and Confirm Password doesn't match" })
         }
       } else {
         res.send({ "status": "failed", "message": "All fields are required" })
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 // Defining Schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
+  UserName: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true },
   password: { type: String, required: true, trim: true }
  
