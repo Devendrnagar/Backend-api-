79d6ef85705d503685f9b9bd24d351b006d3e785
devendra nagar <devendranagar853@gmail.com>
2024-02-26 13:58:29 +0530

    login api add in

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,19 +5,19 @@ import jwt from 'jsonwebtoken'
 class UserController {
   static userRegistration = async (req, res) => {
 
-    const { name,  UserName, email, password, password_confirmation } = req.body
+    const { name, UserName, email, password, password_confirmation } = req.body
 
     console.log({ name, UserName, email, password, password_confirmation, body: req.body })
 
     const user = await UserModel.findOne({ email: email })
 
     if (user) {
-       
-      res.status(403).send({
+
+      res.status(400).send({
         message: "Records show this email is linked to another account."
       });
 
-      
+
 
     }
 
@@ -36,10 +36,10 @@ class UserController {
 
             const doc = new UserModel({
               name: name,
-              UserName:UserName,
+              UserName: UserName,
               email: email,
               password: hashPassword,
-              
+
             })
 
             await doc.save()
@@ -55,10 +55,10 @@ class UserController {
           } catch (error) {
             console.log(error)
 
-            res. status(400).send({ "status": "failed", "message": "Unable to Register" })
+            res.status(400).send({ "status": "failed", "message": "Unable to Register" })
           }
         } else {
-          res.status(). send({ "status": "failed", "message": "Password and Confirm Password doesn't match" })
+          res.status().send({ "status": "failed", "message": "Password and Confirm Password doesn't match" })
         }
       } else {
         res.send({ "status": "failed", "message": "All fields are required" })
@@ -66,7 +66,37 @@ class UserController {
     }
   }
 
-  
+
+  // login from 
+
+
+  static userLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body
+      if (email && password) {
+        const user = await UserModel.findOne({ email: email })
+        if (user != null) {
+          const isMatch = await bcrypt.compare(password, user.password)
+          if ((user.email === email) && isMatch) {
+            // Generate JWT Token
+            const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+            res.send({ "status": "success", "message": "Login Success", "token": token })
+          } else {
+            res.send({ "status": "failed", "message": "Email or Password is not Valid" })
+          }
+        } else {
+          res.send({ "status": "failed", "message": "You are not a Registered User" })
+        }
+      } else {
+        res.send({ "status": "failed", "message": "All Fields are Required" })
+      }
+    } catch (error) {
+      console.log(error)
+      res.send({ "status": "failed", "message": "Unable to Login" })
+    }
+  }
+  // end from 
+
 }
 
 export default UserController
